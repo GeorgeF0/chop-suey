893a020e64946d0eba9a89324526e34e12343384
George Flourentzos <georgeflourentzos@gmail.com>
2017-03-30 22:49:24 +0100

    Added script and stylesheet to main page, style needs fixing

--- ChopSuey/Content/script.js
+++ ChopSuey/Content/script.js
@@ -1,1 +1,24 @@
-﻿
+﻿//http://jsfiddle.net/unLSJ/
+
+if (!library) var library = {};
+
+library.json = {
+    replacer: function (match, pIndent, pKey, pVal, pEnd) {
+        const key = "<span class=json-key>";
+        const val = "<span class=json-value>";
+        const str = "<span class=json-string>";
+        var r = pIndent || "";
+        if (pKey)
+            r = r + key + pKey.replace(/[": ]/g, "") + "</span>: ";
+        if (pVal)
+            r = r + (pVal[0] === '"' ? str : val) + pVal + "</span>";
+        return r + (pEnd || "");
+    },
+    prettyPrint: function (obj) {
+        const jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+        return JSON.stringify(obj, null, 3)
+            .replace(/&/g, "&amp;").replace(/\\"/g, "&quot;")
+            .replace(/</g, "&lt;").replace(/>/g, "&gt;")
+            .replace(jsonLine, library.json.replacer);
+    }
+};
